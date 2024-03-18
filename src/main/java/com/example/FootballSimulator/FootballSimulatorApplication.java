61b1ace5d0fb6030cba012d73edf495000f40223
TodorYanakiev <todoryanakiev395@gmail.com>
2024-03-18 19:20:29 +0200

    first commit

---
+++ src/main/java/com/example/FootballSimulator/FootballSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.FootballSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FootballSimulator/FootballSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FootballSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
