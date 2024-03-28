037362fe6d449533f5660c0dfb725aa83e6ad404
TodorYanakiev <todoryanakiev395@gmail.com>
2024-03-29 00:28:41 +0200

    Form for adding bse teams, get method for all teams

---
+++ src/main/java/com/example/FootballSimulator/BaseFootballTeam/BaseFootballTeamRepository.java
@@ -0,0 +1,6 @@
+package com.example.FootballSimulator.BaseFootballTeam;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BaseFootballTeamRepository extends CrudRepository<BaseFootballTeam, Long> {
+}
