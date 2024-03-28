69cceee08a7bec979dc3c1f0ae36dc1297b368c9
TodorYanakiev <todoryanakiev395@gmail.com>
2024-03-28 23:26:50 +0200

    FootballPlayer added

--- src/main/java/com/example/FootballSimulator/FootballPlayer/FootballPlayer.java
+++ src/main/java/com/example/FootballSimulator/FootballPlayer/FootballPlayer.java
@@ -1,6 +1,10 @@
 package com.example.FootballSimulator.FootballPlayer;
 
+import com.example.FootballSimulator.BaseFootballPlayer.BaseFootballPlayer;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class FootballPlayer {
@@ -9,11 +13,140 @@ public class FootballPlayer {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "base_player_id")
+    private BaseFootballPlayer baseFootballPlayer;
+
+    @NotNull
+    @Min(0)
+    private Integer price;
+
+    @NotNull
+    @Min(1)
+    @Max(99)
+    private Byte defending;
+
+    @NotNull
+    @Min(1)
+    @Max(99)
+    private Byte speed;
+
+    @NotNull
+    @Min(1)
+    @Max(99)
+    private Byte dribble;
+
+    @NotNull
+    @Min(1)
+    @Max(99)
+    private Byte scoring;
+
+    @NotNull
+    @Min(1)
+    @Max(99)
+    private Byte passing;
+
+    @NotNull
+    @Min(1)
+    @Max(99)
+    private Byte stamina;
+
+    @NotNull
+    @Min(1)
+    @Max(99)
+    private Byte positioning;
+
+    @NotNull
+    @Min(1)
+    @Max(99)
+    private Byte goalkeeping;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public BaseFootballPlayer getBaseFootballPlayer() {
+        return baseFootballPlayer;
+    }
+
+    public void setBaseFootballPlayer(BaseFootballPlayer baseFootballPlayer) {
+        this.baseFootballPlayer = baseFootballPlayer;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Byte getDefending() {
+        return defending;
+    }
+
+    public void setDefending(Byte defending) {
+        this.defending = defending;
+    }
+
+    public Byte getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Byte speed) {
+        this.speed = speed;
+    }
+
+    public Byte getDribble() {
+        return dribble;
+    }
+
+    public void setDribble(Byte dribble) {
+        this.dribble = dribble;
+    }
+
+    public Byte getScoring() {
+        return scoring;
+    }
+
+    public void setScoring(Byte scoring) {
+        this.scoring = scoring;
+    }
+
+    public Byte getPassing() {
+        return passing;
+    }
+
+    public void setPassing(Byte passing) {
+        this.passing = passing;
+    }
+
+    public Byte getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(Byte stamina) {
+        this.stamina = stamina;
+    }
+
+    public Byte getPositioning() {
+        return positioning;
+    }
+
+    public void setPositioning(Byte positioning) {
+        this.positioning = positioning;
+    }
+
+    public Byte getGoalkeeping() {
+        return goalkeeping;
+    }
+
+    public void setGoalkeeping(Byte goalkeeping) {
+        this.goalkeeping = goalkeeping;
     }
 }
---
+++ src/main/java/com/example/FootballSimulator/FootballPlayer/FootballPlayerRepository.java
@@ -0,0 +1,6 @@
+package com.example.FootballSimulator.FootballPlayer;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FootballPlayerRepository extends CrudRepository<FootballPlayer, Long> {
+}
