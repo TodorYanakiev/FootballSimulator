e40a06a5fe4264e9e6d1db2e50b844fa32c973aa
Echtaaa07 <atche120207@gmail.com>
2024-04-03 16:18:16 +0300

    football players added

--- src/main/java/com/example/FootballSimulator/BaseFootballPlayer/BaseFootballPlayerRepository.java
+++ src/main/java/com/example/FootballSimulator/BaseFootballPlayer/BaseFootballPlayerRepository.java
@@ -1,6 +1,10 @@
 package com.example.FootballSimulator.BaseFootballPlayer;
 
+import com.example.FootballSimulator.BaseFootballTeam.BaseFootballTeam;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BaseFootballPlayerRepository extends CrudRepository<BaseFootballPlayer, Long> {
+    List<BaseFootballPlayer> findAllByIdIn(List<Long> ids);
 }
